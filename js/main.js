6c05a514a76ecf2c489ba05581fc523a2896c98a
frsmtv <fsimitchiev@gmail.com>
2016-12-27 03:09:53 +0100

    improvements made on homepage, waiting for the other pages...

--- js/main.js
+++ js/main.js
@@ -41,6 +41,8 @@ $(document).ready(function() {
                     $('span').css('font-size', '1rem');
                     $('mark').css('font-size', '1.4rem');
                     $('#contact').show();
+                    $('.menuLink').addClass('animated fadeInUp');
+                    $('#logo').css('width', '120px');
                     // $(window).scrollTop($(document).height());
                 }
             }
