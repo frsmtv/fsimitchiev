7062742da5c1e023570119313533cb3df1639501
frsmtv <fsimitchiev@gmail.com>
2017-01-06 16:04:30 +0100

    added favicon

--- js/main.js
+++ js/main.js
@@ -1,48 +1,9 @@
 $(document).ready(function() {
 
-    // ********** TEXT ANIM T.JS **********
-//     var fin=!1;
-//
-//     function foo(){
-//         $('mark').eq(0).css({background:'magenta'});
-//     }
-//
-//     $(function(){
-//         $('#t').t({
-//             speed:15,
-//             speed_vary:true,
-//             mistype:00,
-//
-//             typing:function(elem,chars_total,chars_left,_char){
-//                 if(_char=='*')foo();
-//                 // Auto scroll to top
-//                 $(window).scrollTop($(document).height());
-//
-//             },
-//
-//             fin:function(){
-//
-//                 if(fin==!1){
-//                     fin=!!1; //avoids triggering after 'add' cmd
-// //                    window.setTimeout(function(){$('#t').t('add','<br>Clique !');},2e4);
-//                     window.setInterval(function(){$('#t').find('.t-caret').toggle();},5e2);
-//
-//                     // Show menu after intro
-//                     // $('#fullpage').css({"-webkit-transform":"translate(0px,-50px)"})
-//                     // $('span').css('font-size', '1rem');
-//                     // $('mark').css('font-size', '1.4rem');
-//                     $('#contact').show();
-//                     $('footer').fadeIn();
-//                     // $('#logo').css('width', '120px');
-//                     // $(window).scrollTop($(document).height());
-//                 }
-//             }
-//         });
-//     });
 
-    // // ********** TEXT ANIM BUBBLE-Y **********
+    // ********** TEXT ANIM BUBBLE-Y **********
     var $quotes = $('#quotes .quote'),
-        opts = { fadeTime: 1000, dwellTime: 6000 },
+        opts = { fadeTime: 1000, dwellTime: 4500 },
         shuffle,
         fadeInQuote,
         fadeOutQuote,
@@ -127,26 +88,6 @@ $(document).ready(function() {
         } else {
             $('#logo').removeClass('zoomOut').addClass('zoomIn');
         }
-    });
-
-    // *********** PROJETS PAGE ANIMATIONS ***********
-
-    // **** MUSIQUE SECTION ****
-    $('#musicbtn').click(function() {
-        $('#musicbtn').css('opacity', '1');
-       $('#webbtn').css('opacity', '0.5');
-        $('#web').hide();
-       $('#musique').fadeIn();
-       $('footer').show();
-    });
-
-    // **** WEB SECTION ****
-    $('#webbtn').click(function() {
-        $('#musicbtn').css('opacity', '0.5');
-        $('#webbtn').css('opacity', '1');
-        $('#musique').hide();
-        $('#web').fadeIn();
-        $('footer').show();
     });
 
 });
